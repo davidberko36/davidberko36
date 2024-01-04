d0f009415d27ac7f39c37a227a318765a4bf06fc
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-01-04 05:17:36 +0000

    04/01/2024

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Landing from './landing';
+
+function App() {
+  return (
+    <div className="App min-h-screen  justify-center">
+      <Landing/>     
+    </div>
+  );
+}
+
+export default App;
